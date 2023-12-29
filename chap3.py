5995b60031dd90c883bec12c2d6dab86cc354c48
nezatiop <155076595+nezatiop@users.noreply.github.com>
2023-12-29 20:33:49 +0100

    Update chap3.py

--- chap3.py
+++ chap3.py
@@ -48,3 +48,19 @@ mots = phrase.split()
 for mot in mots:
     print(mot)
 """
+"""
+liste_entiers = input("Entrez une liste d'entiers (séparés par des espaces) : ").split()
+
+valeur_recherchee = input("Entrez la valeur à rechercher : ")
+
+indices = []
+
+for i in range(len(liste_entiers)):
+    if liste_entiers[i] == valeur_recherchee:
+        indices.append(i)
+
+if indices:
+    print(f"La valeur {valeur_recherchee} apparaît aux indices : {indices}")
+else:
+    print(f"La valeur {valeur_recherchee} n'apparaît pas dans la liste.")
+"""
